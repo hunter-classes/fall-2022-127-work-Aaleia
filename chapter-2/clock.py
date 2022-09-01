68e0f131e14f6495ad7a28950bbd296cefc5d298
Aaleia <104999170+Aaleia@users.noreply.github.com>
2022-09-01 03:40:17 +0000

    Created new folder chapter-2

---
+++ chapter-2/clock.py
@@ -0,0 +1,2 @@
+## If it is currently 13 and you set your alarm to go off in 50 hours, it will be 15 (3pm). Write a Python program to solve the general version of the above problem. Ask the user for the time now (in hours), and then ask for the number of hours to wait for the alarm. Your program should output what the time will be on the clock when the alarm goes off.
+
