ef7302424a1fd9f5129ecbadc72c4028995a8a52
Aaleia <104999170+Aaleia@users.noreply.github.com>
2022-09-08 09:50:09 +0000

    added chapter_4

---
+++ chapter_4/three.py
@@ -0,0 +1,4 @@
+## Write a program that uses a for loop to print "One of the months of the year is January" etc ...
+
+for a in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']:
+    print("One of the months of the year is", a)
