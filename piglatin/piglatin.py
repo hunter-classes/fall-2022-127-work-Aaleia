d4184a3e0f222dc5a88c87bcf4abb7b4af124f75
Aaleia <104999170+Aaleia@users.noreply.github.com>
2022-09-19 10:51:26 +0000

    added piglatin.py

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,22 +1,19 @@
 def bondify(name):
-  result = ""
-  space = name.find(' ')
-  first = 
-
-"""
-input: a string representing a word
-returns: a new string with the word converted to piglatin
-
-Rules:
-if the first letter is a consonent, move it from the start to the end and add "ay"
-so "hello" becomes "ellohay"
-
-If the first letter is a vowel, just add "yay" to the end
-
-try to also handle upper case words
-
-"""
-# Make sure to test from the bottom of your program - when run, it should call piglatin on various words and print out the word and the results
+    result = ""
+    space = name.find(" ")
+    first = name[0].upper() + name[1:space]
+    last = name[space+1].upper() + name[space+2:]
+    result = last + ", " + first + " " + last
+    return result
+print(bondify("james bond"))
 
 def piglatin(word):
-  
+    if word[0] in "a,e,i,o,u" :
+        return word + "yay"
+    else:
+        return word [1:] + word[0] + "ay" 
+
+result = piglatin("create")
+print(result)
+result = piglatin("bond")
+print(result)
