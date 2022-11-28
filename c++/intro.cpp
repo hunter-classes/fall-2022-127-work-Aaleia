b3ad9cca205d5504bc821be15e782c0c5bed962e
Aaleia <104999170+Aaleia@users.noreply.github.com>
2022-11-28 14:29:32 +0000

    added c++ folder and intro file

---
+++ c++/intro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// this is a single-line comment
+
+/*
+  This is a
+  multi-line
+  comment
+
+  to compile:
+  g++ intro.cpp
+  or
+  g++ -o intro intro.cpp
+
+to run:
+
+*/
+
+int main()
+{
+  std::cout << "Hello World" << std::endl;
+  
+  return 0;
+}
