ca66f2da5907e38dbb2bb35052ee52ce0d327f5c
Aaleia <104999170+Aaleia@users.noreply.github.com>
2022-12-15 12:00:10 +0000

    added main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,27 @@
+#include <iostream>
+
+a = 5
+b = 3
+int add(int a , int b){
+  int c;
+  std::cout << "In func a: " << a << " b: " << b << "/n";
+  c = a + b;
+  return c;
+}
+
+void oddoreven(int num)
+{
+if(num % 2 == 0)
+{
+  std::cout<<"Even\n";
+}
+else
+{
+  std::cout<<"Odd\n"; 
+}
+
+
 int main()
 {
-  std::cout << "Hello World" << std::endl;
-
+  std::cout << "Hello World\n" << std::endl;
 }
